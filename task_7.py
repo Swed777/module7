961c1a53a2011f46d8acec9b0ce9a2097c3796e9
Swed777 <107791675+Swed777@users.noreply.github.com>
2022-08-03 12:26:34 +0000

    7_module_finish

--- task_7.py
+++ task_7.py
@@ -7,9 +7,12 @@ print('-------------------')
 
 a = int(input('Введите число a: '))
 b = int(input('Введите число b: '))
+count_number = 0
+summ_3 = 0
 
-for nnn in range(a,b):
-  
-  
-  
-  print(a,b)
+for number in range(a,b):
+  if number % 3 == 0:
+    summ_3 += number
+    count_number += 1  
+print('Среднее арифметическое чисел, кратных 3 =', summ_3 / count_number)
+print('-------------------')
--- task_8.py
+++ task_8.py
@@ -4,3 +4,9 @@ print('Задача 8. Замечательные числа')
 # которая находит и выводит все двузначные числа,
 # которые равны утроенному произведению своих цифр.
 # К таким относятся, например, 15 и 24.
+
+for number in range(10, 100):
+  first_figure = number // 10
+  second_figure = number % 10
+  if number == first_figure * second_figure * 3:
+    print(number)
